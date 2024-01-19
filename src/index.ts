78d236a01d34f5f3b057e2ddd64884f32d6bbad7
Brian Wong <brianw7218@gmail.com>
2024-01-18 22:12:34 -0800

    Refactor to not be overly dependent on Apollo stuff so that the endpoint can be changed

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,35 @@
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { expressMiddleware } from "@apollo/server/express4";
 import "dotenv/config";
-import { generateContent } from "./resolvers/generateContent.js";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import { typeDefs } from "./schema/schema.js";
+import { resolvers } from "./resolvers/query.js";
 
+const app = express();
+const httpServer = http.createServer(app);
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-// Passing an ApolloServer instance to the `startStandaloneServer` function:
-//  1. creates an Express app
-//  2. installs your ApolloServer instance as middleware
-//  3. prepares your app to handle incoming requests
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
-});
+await server.start();
 
-console.log(`🚀  Server ready at: ${url}`);
+app.use(
+  "/graphql",
+  cors(),
+  express.json(),
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ token: req.headers.token }),
+  })
+);
+
+const port = process.env.PORT || 4000;
+
+httpServer.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
