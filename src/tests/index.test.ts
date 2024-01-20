5623af5438798c82a1ec807d08a63e5505854d7f
Brian Wong <brianw7218@gmail.com>
2024-01-19 23:16:13 -0800

    Add test for server

---
+++ src/tests/index.test.ts
@@ -0,0 +1,29 @@
+import { ApolloServer } from "@apollo/server";
+import { ApolloServerContext } from "../types/apolloServerContext";
+import { readFileSync } from "fs";
+import { resolvers } from "../resolvers/query";
+
+test("The server responds with generated text", async () => {
+  const typeDefs = readFileSync("./src/schema/schema.graphql", {
+    encoding: "utf-8",
+  });
+
+  const server = new ApolloServer<ApolloServerContext>({
+    typeDefs,
+    resolvers,
+  });
+
+  const response = await server.executeOperation({
+    query:
+      "query Query($duration: Int!) { aiText(duration: $duration) {content}}",
+    variables: { duration: 20 },
+  });
+
+  expect(response).toHaveProperty([
+    "body",
+    "singleResult",
+    "data",
+    "aiText",
+    "content",
+  ]);
+});
