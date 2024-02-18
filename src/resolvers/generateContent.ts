edd46f48687e36d6eb7622af00b21e9ec4d6bbb2
Brian Wong <brianw7218@gmail.com>
2024-02-17 22:25:13 -0800

    Update the Vertex AI API and fix breaking changes

--- src/resolvers/generateContent.ts
+++ src/resolvers/generateContent.ts
@@ -42,27 +42,27 @@ const generateContent = async (readingDuration: Number) => {
         ],
       },
     ],
-    generation_config: {
-      max_output_tokens: 2048,
+    generationConfig: {
+      maxOutputTokens: 2048,
       temperature: 1,
-      top_p: 1,
+      topP: 1,
     },
-    safety_settings: [
+    safetySettings: [
       {
         category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-        threshold: HarmBlockThreshold.BLOCK_NONE,
+        threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
       },
       {
         category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-        threshold: HarmBlockThreshold.BLOCK_NONE,
+        threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
       },
       {
         category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-        threshold: HarmBlockThreshold.BLOCK_NONE,
+        threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
       },
       {
         category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-        threshold: HarmBlockThreshold.BLOCK_NONE,
+        threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
       },
     ],
   } as GenerateContentRequest;
