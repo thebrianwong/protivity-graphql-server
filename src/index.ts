e29a93d5ee206fb313a986957ffb66090d2ab64d
Brian Wong <brianw7218@gmail.com>
2024-03-31 21:04:20 -0700

    Add type and change index from an Express server to the format for AWS Lambda

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import express from "express";
-import http from "http";
-import cors from "cors";
+import { generateContent } from "./generateContent.js";
+import RequestBody from "./types/RequestBody.type";
 
-const app = express();
-const httpServer = http.createServer(app);
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/", async (req, res, next) => {
-  res.send("This is a GraphQL server. Make a request at /graphql");
-});
-
-const port = process.env.PORT || 4000;
-
-httpServer.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+export const handler = async (event: RequestBody) => {
+  try {
+    const content = await generateContent(event.duration);
+    const response = {
+      statusCode: 200,
+      body: content,
+    };
+    return response;
+  } catch (e) {
+    const response = {
+      statusCode: 500,
+      error: e,
+    };
+    return response;
+  }
+};
---
+++ src/types/RequestBody.type.ts
@@ -0,0 +1,5 @@
+type RequestBody = {
+  duration: Number;
+};
+
+export default RequestBody;
